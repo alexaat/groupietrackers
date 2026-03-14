648012d68c96d2650e327771d2a6ac23a58d5353
aliakseividaseu <alexaat@yahoo.co.uk>
2026-03-14 11:02:51 +0000

    deployment setup

--- api/index.go
+++ api/index.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"embed"
 	"fmt"
 	"net/http"
 )
+
+//go:embed templates/*
+var templates embed.FS
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Main Page</h1>")
